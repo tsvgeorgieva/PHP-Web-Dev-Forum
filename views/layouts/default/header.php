f587c2d24a554c4768d2ec291f0c56bb4acf5a4e
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2015-05-08 19:41:40 +0300

    added Nakov's framework for start

---
+++ views/layouts/default/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="/content/styles.css" />
+    <title>
+        <?php if (isset($this->title)) echo htmlspecialchars($this->title) ?>
+    </title>
+</head>
+
+<body>
+    <header>
+        <a href="/"><img src="/content/images/site-logo.png"></a>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/authors">Authors</a></li>
+            <li><a href="/books">Books</a></li>
+        </ul>
+    </header>
+
+    <?php include('messages.php'); ?>
